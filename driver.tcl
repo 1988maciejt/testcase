e2138df9df6dfdb7f34c4f8d79fd6c45977b07bc
Maciej Trawka <maciej@MSI.localdomain>
2021-10-21 20:59:49 +0200

    First commit

---
+++ driver.tcl
@@ -0,0 +1,89 @@
+set SKETCH_NAME	my_sketch
+set DEV_NAME	my_device
+set BAUD	9600
+set BOARD	arduino:avr:nano:cpu=atmega328old	
+
+### CREATE ARDUINO SKETCH
+set a [arduino_create $SKETCH_NAME $BOARD]
+
+### ADD REQUIRED FUNCTIONS
+arduino_function_add_from_template a int2string
+
+### SECONDS COUNTER
+arduino_global_declaration_add a "int ss = 0;"
+arduino_timer_set_periodic_lines a 1000 [list \
+	"ss += 1;" ]
+
+## LCD I2C DISPLAY
+arduino_lcd_i2c_add a
+arduino_setup_lines_add a [list \
+	[arduino_line_lcd_set_cursor 0 0] \
+	[arduino_line_lcd_print "\"Hello World!\""] ]
+arduino_timer_set_periodic_lines a 250 [list \
+	[arduino_line_lcd_set_cursor 3 1] \
+	[arduino_line_lcd_print "\"SS\=\""] \
+	[arduino_line_lcd_print "int2string(ss, 2, true)"] ]
+	
+## LED
+arduino_definition_add a LED 13
+arduino_output_add a LED
+arduino_timer_set_periodic_lines a 500 [list \
+	[arduino_line_output_toggle LED] ]
+	
+## SERIAL INTERPRETER
+arduino_serial_init a $BAUD 
+arduino_serial_interpreter_add a [list \
+	"if (argc.size() > 0) \{" 				\
+	"  String cmd = argc\[0];" 				\
+	"  if (cmd == \"led\") \{" 				\
+	"    uint8_t val = argc\[1].toInt();" 	\
+	"    pinMode (LED, OUTPUT);" 			\
+	"    digitalWrite (LED, val);" 			\
+	"    Serial.println (\"ok\");" 			\
+	"  }" 									\
+	"  else if (cmd == \"set\") \{" 		\
+	"    uint8_t pin = argc\[1].toInt();" 	\
+	"    uint8_t val = argc\[2].toInt();" 	\
+	"    pinMode (pin, OUTPUT);" 			\
+	"    digitalWrite (pin, val);" 			\
+	"    Serial.println (\"ok\");" 			\
+	"  }" 									\
+	"  else if (cmd == \"get\") \{" 		\
+	"    uint8_t pin = argc\[1].toInt();" 	\
+	"    uint8_t val = digitalRead (pin);" 	\
+	"    Serial.println (val);" 			\
+	"  }" 									\
+	"}" 									] \
+	-d $DEV_NAME
+	
+arduino_function_create_voltage_measure a myVoltage A0 -max 12 -rl 22000
+arduino_class_add_from_template a fifo
+
+
+
+## compilation and upload ##
+arduino_sketch_compile a 
+
+set sketch_diff [diff [arduino_sketch_filename results my_sketch]]
+
+set port [serial_first]
+if {$port eq 0} {
+	print "No serial device found."
+} else {
+	if {$sketch_diff} {
+		print "Upload sketch..."
+		arduino_sketch_upload a $BOARD $port 
+	}
+	set s [serial_open_for_dev_name $DEV_NAME $BAUD]
+	if {$s eq 0} {
+		exit
+	}
+	serial_close $s
+	wait 1000
+	serial_open $port $BAUD
+	serial_monitor $s 3000
+	print "Starting test"
+	set test_list [file_to_list_of_lists data/test_data.txt]
+	serial_test $s $test_list
+	serial_close $s
+}
---
+++ references/results/my_sketch/my_sketch_MSI_maj-25-194715-2021_Conflict.ino
@@ -0,0 +1,168 @@
+#include <LinkedList.h>
+#include <LiquidCrystal_I2C.h>
+#include <SimpleTimer.h>
+
+#define LED 	 13
+#define MYVOLTAGE_COEFF_A 	 0.053763440860215055
+#define MYVOLTAGE_COEFF_B 	 0.0
+
+String int2string (int x, int positions, bool leading_zeros = false);
+void timer_1000ms_handler0 ();
+void timer_250ms_handler1 ();
+void timer_500ms_handler2 ();
+void serialParserCallback (LinkedList<String> &argc);
+void serialHandler ();
+void serialParser ();
+LinkedList<String> splitString (String text, char separator = ' ');
+String getValue (String data, char separator, int index);
+double analogReadLinScale (uint8_t analog_input, double a, double b);
+double myVoltage ();
+
+LiquidCrystal_I2C lcd (0x27, 16, 2);
+SimpleTimer main_timer;
+String serial_buffer = "";
+int int_timer_1000ms_handler0_1000 = -1;
+int int_timer_250ms_handler1_250 = -1;
+int int_timer_500ms_handler2_500 = -1;
+int ss = 0;
+
+void setup () {
+  Serial.begin (9600);
+  int_timer_1000ms_handler0_1000 = main_timer.setInterval (1000, timer_1000ms_handler0);
+  lcd.init ();
+  lcd.backlight ();
+  lcd.setCursor (0, 0);
+  lcd.print ("Hello World!");
+  int_timer_250ms_handler1_250 = main_timer.setInterval (250, timer_250ms_handler1);
+  pinMode (LED, OUTPUT);
+  digitalWrite (LED, 0);
+  int_timer_500ms_handler2_500 = main_timer.setInterval (500, timer_500ms_handler2);
+}
+
+void loop () {
+  main_timer.run();
+  serialHandler ();
+}
+
+String int2string (int x, int positions, bool leading_zeros) {
+  bool minus = (x < 0);
+  if (minus) x = abs(x);
+  String r = String(x, DEC);
+  if (minus && leading_zeros) positions--;
+  else if (minus) r = '-' + r;
+  while (r.length() < positions) r = (leading_zeros ? '0' : ' ') + r;
+  if (minus && leading_zeros) r = '-' + r;
+  return r;
+}
+
+void timer_1000ms_handler0 () {
+  ss += 1;
+}
+
+void timer_250ms_handler1 () {
+  lcd.setCursor (3, 1);
+  lcd.print ("SS=");
+  lcd.print (int2string(ss, 2, true));
+}
+
+void timer_500ms_handler2 () {
+  digitalWrite (LED, !digitalRead(LED));
+}
+
+void serialParserCallback (LinkedList<String> &argc) {
+  if (argc[0] == "dev_name") {
+    Serial.println ("my_device");
+  }
+  if (argc[0] == "version") {
+    Serial.println ("1.0.0");
+  }
+  if (argc.size() > 0) {
+    String cmd = argc[0];
+    if (cmd == "set") {
+      uint8_t pin = argc[1].toInt();
+      uint8_t val = argc[2].toInt();
+      pinMode (pin, OUTPUT);
+      digitalWrite (pin, val);
+      Serial.println ("ok");
+    }
+    else if (cmd == "get") {
+      uint8_t pin = argc[1].toInt();
+      uint8_t val = digitalRead (pin);
+      Serial.println (val);
+    }
+  }
+}
+
+void serialHandler () {
+  while (Serial.available() > 0) {
+    char received_chr = Serial.read();
+    switch (received_chr) {
+      case '\r':  // CR - parse
+        serialParser();
+        break;
+      case '\n':  // LF - parse
+        serialParser();
+        break;
+      case 0:     // null - parse
+        serialParser();
+        break;
+      case 8:     // backspace - delete last char
+        serial_buffer.remove(serial_buffer.length() - 1);
+        break;
+      default:
+        serial_buffer += received_chr; 
+    }
+  }
+}
+
+void serialParser () {
+  if (serial_buffer.length() < 1) return;
+  serial_buffer.trim ();
+  serial_buffer.replace ("  ", " ");
+  serial_buffer.replace ("\t", " ");
+  serial_buffer.replace ("\n", " ");
+  serial_buffer.replace ("  ", " ");
+  LinkedList<String> list = splitString (serial_buffer, ' ');
+  serialParserCallback (list);
+  list.clear();
+  serial_buffer = "";
+}
+
+LinkedList<String> splitString (String text, char separator) {
+  LinkedList<String> list;
+  for (byte i = 0; i < 100; ++i) {
+    String x = getValue(text, separator, i);
+    if (x.length() == 0) break;
+    list.add(x);
+  }
+  return list;
+}
+
+String getValue (String data, char separator, int index) {
+  int found = 0;
+  int strIndex[] = { 0, -1 };
+  int maxIndex = data.length() - 1;
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+double analogReadLinScale (uint8_t analog_input, double a, double b) {
+  double voltage;
+  return ((double)analogRead(analog_input) * a + b);
+}
+
+double myVoltage () {
+  double sum = 0;
+  for (unsigned int i = 0; i < 10; ++i) {
+    sum += analogReadLinScale (A0, MYVOLTAGE_COEFF_A, MYVOLTAGE_COEFF_B);
+  }
+  return (sum / 10);
+}
+
+
