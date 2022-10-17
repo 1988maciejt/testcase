2c883a1cf1123b33e1d0fa92fdd46b94508c9dc3
Maciej Trawka <maciejtr@gmail.com>
2022-10-17 20:52:10 +0200

    update

--- driver.py
+++ driver.py
@@ -5,17 +5,16 @@ from aio import *
 # place your code here
 
 SizeList = [i for i in range(4,33)]
-CoefficientsList = [3,4,5,6]
+CoefficientsList = [3,4,5]
 FanoutMax = 2
 MaxAndCount = 3
-MinCountPerSize = 3
 InvertersAllowed = 1
 LayoutFriendly = 1
 
 file = open('nlfsrs.txt', 'w')
 
 for Size in SizeList:
-  sresults = []
+  Aio.print(f'Size: {Size}')
   for Coefficients in CoefficientsList:
     print (f'// Size = {Size}, Coefficients = {Coefficients}')
     if (Coefficients) + 1 > Size:
@@ -27,28 +26,27 @@ for Size in SizeList:
     for poly in polys:
       if not (poly.getReversed() in polylist): 
         polylist.append(poly.copy())
-    while len(polylist) > 0:
-      poly = polylist[int(uniform(0, len(polylist)))]
-      polylist.remove(poly)
+    cresults = []
+    for poly in polylist:
       print(f'// poly: {poly}')
       results = Nlfsr.findNLRGsWithSpecifiedPeriod(poly, InvertersAllowed=InvertersAllowed, MaxAndCount=MaxAndCount)
-      cresults = []
       for i in range(len(results)):
         for FM in range(2, FanoutMax+1):
           if results[i].makeBeauty(FM):
             cresults.append(results[i])
             break
-      sresults += cresults
-      sresults = Nlfsr.filterEquivalent(sresults)
-      sresults = Nlfsr.filterInverted(sresults)
-      if (len(sresults)) >= MinCountPerSize:
-        break
-    if len(sresults) >= MinCountPerSize:
-      break
-  Aio.print(f'Size: {Size}')
-  for R in sresults:
-    Aio.print(R.getFullInfo().replace('\n', ', \t'))
-    file.write(repr(R) + '\n')
-  file.flush()
+      print(f'// FOund: {len(cresults)}')
+    cresults = Nlfsr.filterEquivalent(cresults)
+    cresults = Nlfsr.filterInverted(cresults)
+    Lines = []
+    for R in cresults:
+      Line = R.toBooleanExpressionFromRing()
+      Line += " " * (60 - len(Line)) +  R.getFullInfo(Header=0).replace('\n', ',   ')
+      Lines.append(Line)
+      file.write(repr(R) + '\n')
+    file.flush()
+    Lines.sort()
+    for Line in Lines:
+      Aio.print(Line)
   Aio.print('')
 file.close()
