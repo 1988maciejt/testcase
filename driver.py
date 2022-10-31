9cb42d464cb79509fc0dbb2de079d39625a6b223
Maciej Trawka <maciejtr@gmail.com>
2022-10-31 22:16:16 +0100

    update

--- driver.py
+++ driver.py
@@ -4,12 +4,11 @@ from aio import *
 
 
 SIZES = [16, 24, 32, 48, 64, 96, 128, 192, 256]
-USE_DEMUXES = 1
 
-Aio.print(f'Size \tDense \tDemuxes \t#Found')
-for Size in SIZES:
-  for USE_DEMUXES in [0, 1]:
-    for DENSE in [0, 1]:
+Aio.print(f'Demuxes \tDense \tSize \t#Found')
+for USE_DEMUXES in [1, 0]:
+  for DENSE in [0, 1]:
+    for Size in SIZES:
       Step = 3 if DENSE else 4
       Taps = []
       for S in range(Size-1, (Size>>1), -Step):
@@ -34,7 +33,7 @@ for Size in SIZES:
           Taps.append({f'{S}-{D2}_off': None, f'{S}-{D2}_on': [S,D2]})
           Taps.append({f'{S}-{D3}_off': None, f'{S}-{D3}_on': [S,D3]})
       Found = Lfsr.listMaximumLfsrsHavingSpecifiedTaps(Size, Taps)
-      Aio.print(f'{Size} \t{DENSE} \t{USE_DEMUXES} \t{len(Found)}')
+      Aio.print(f'{USE_DEMUXES} \t{DENSE} \t{Size} \t{len(Found)}')
     #  for F in Found:
     #    print(repr(F), F.MuxConfig)
     
