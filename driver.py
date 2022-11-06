d45222c8ed52025894d2f2e43ee0d60c4390adcb
Maciej Trawka <maciejtr@gmail.com>
2022-11-06 09:22:16 +0100

    faster

--- driver.py
+++ driver.py
@@ -2,37 +2,45 @@
 from aio import *
 # ============================
 
-# place your code here
-SIZES = [i for i in range(8, 16+1)]
+
+SIZES = [i for i in range(8, 32+1)]
 
 
-Aio.print(f'Size \t#Found')
+Aio.print(f'Size \t#Found \t#Polys \t#UTaps \t Unused taps')
 for Size in SIZES:
+#    Aio.print(f'\nSIZE: {Size}')
     Taps = []
     UpperCounter = (Size>>1) + 1 + (Size&1)
     LowerCounter = (Size>>1) - 2
+    AllTaps = []
     if (Size&1):
         S = (Size>>1) 
         D = (Size>>1) 
+        AllTaps.append([S,D])
         Tap = {f'{S}-{D}_off' : None, f'{S}-{D}_on': [S, D]}
         Taps.append(Tap)
     S = UpperCounter
     D = LowerCounter + 1
+    AllTaps.append([S,D])
     Tap = {f'{S}-{D}_off' : None, f'{S}-{D}_on': [S, D]}
     Taps.append(Tap)
     while 1:
         S = UpperCounter
         D = LowerCounter
+        AllTaps.append([S,D])
         Tap = {f'{S}-{D}_off' : None, f'{S}-{D}_on': [S, D]}
         Taps.append(Tap)
         D = LowerCounter - 1
+        AllTaps.append([S,D])
         Tap = {f'{S}-{D}_off' : None, f'{S}-{D}_on': [S, D]}
         Taps.append(Tap)
         S = LowerCounter
         D = UpperCounter
+        AllTaps.append([S,D])
         Tap = {f'{S}-{D}_off' : None, f'{S}-{D}_on': [S, D]}
         Taps.append(Tap)
         D = UpperCounter - 1
+        AllTaps.append([S,D])
         Tap = {f'{S}-{D}_off' : None, f'{S}-{D}_on': [S, D]}
         Taps.append(Tap)
         UpperCounter += 2
@@ -42,34 +50,41 @@ for Size in SIZES:
     if LowerCounter < 0:
         S = LowerCounter + 2
         D = UpperCounter - 1
+        AllTaps.append([S,D])
         Tap = {f'{S}-{D}_off' : None, f'{S}-{D}_on': [S, D]}
         Taps.append(Tap)
     if LowerCounter == 0:
         S = LowerCounter
         D = UpperCounter - 1
+        AllTaps.append([S,D])
         Tap = {f'{S}-{D}_off' : None, f'{S}-{D}_on': [S, D]}
         Taps.append(Tap)
         S = UpperCounter
         D = LowerCounter
+        AllTaps.append([S,D])
         Tap = {f'{S}-{D}_off' : None, f'{S}-{D}_on': [S, D]}
         Taps.append(Tap)
         D = UpperCounter
+        AllTaps.append([S,D])
         Tap = {f'{S}-{D}_off' : None, f'{S}-{D}_on': [S, D]}
         Taps.append(Tap)
     if LowerCounter == -2:
         S = LowerCounter + 2
         D = UpperCounter - 1
+        AllTaps.append([S,D])
         Tap = {f'{S}-{D}_off' : None, f'{S}-{D}_on': [S, D]}
         Taps.append(Tap)
         S = UpperCounter
         D = UpperCounter
+        AllTaps.append([S,D])
         Tap = {f'{S}-{D}_off' : None, f'{S}-{D}_on': [S, D]}
         Taps.append(Tap)
         D = 0
+        AllTaps.append([S,D])
         Tap = {f'{S}-{D}_off' : None, f'{S}-{D}_on': [S, D]}
         Taps.append(Tap)
 #    for Tap in Taps:
-#        Aio.print(list(Tap.keys())[1])
+#        Aio.print(str(list(Tap.keys())[1])[:-3])
 #    break
-    Found = Lfsr.listMaximumLfsrsHavingSpecifiedTaps(Size, Taps, CountOnly=1)
-    Aio.print(f'{Size} \t{Found}')
+    PL = ProgrammableRingGenerator(Size, Taps)
+    Aio.print(f'{Size} \t{len(PL.getLfsrs(False))} \t{len(PL.getPolynomials())} \t{len(PL.getUsedTaps())}/{len(PL.getAllPosssibleTaps())} \t {PL.getUnusedTaps()}')
