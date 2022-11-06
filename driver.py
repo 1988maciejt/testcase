0b191aeb0cf12d4438ef82e98052b3981504d7e1
Maciej Trawka <maciejtr@gmail.com>
2022-11-06 20:55:27 +0100

    fix

--- driver.py
+++ driver.py
@@ -6,47 +6,38 @@ from aio import *
 SIZES = [i for i in range(8, 32+1)]
 
 
-Aio.print(f'Demux \tDense \tSize \t#Found \t#Polys \t#UTaps \t Unused taps')
+Aio.print(f'Demux \tSize \t#Found \t#Polys \t#UTaps \t Unused taps')
 for USE_DEMUXES in [0]:
-  for DENSE in [1]:
-    for Size in SIZES:
-      Step = 3 if DENSE else 4
-      Taps = []
-      AllTaps = []
-      for S in range(Size-1, (Size>>1), -Step):
-        D3 = Size - S
-        D1 = ((D3 - 2) + Size) % Size
-        D2 = ((D3 - 1) + Size) % Size
-        if USE_DEMUXES:
-          Taps.append({f'{S}_Off':None, f'{S}-{D1}':[S,D1], f'{S}-{D2}':[S,D2], f'{S}-{D3}':[S,D3]})
-        else:
-          AllTaps.append([S, D1])
-          AllTaps.append([S, D2])
-          AllTaps.append([S, D3])
-          Taps.append({f'{S}-{D1}_off': None, f'{S}-{D1}_on': [S,D1]})
+  for Size in SIZES:
+    Step = 3
+    Sources = []
+    Taps = []
+    for S in range(Size-1, (Size>>1)-1, -Step):
+      Sources.append(S)
+    for S in range(Step-1, (Size>>1), Step):
+      Sources.append(S)
+    for S in Sources:
+      D3 = Size - S
+      D1 = ((D3 - 2) + Size) % Size 
+      D2 = ((D3 - 1) + Size) % Size
+      #print(S, D1, D2, D3)
+      if USE_DEMUXES:
+        TapSet = {f'{S}_Off':None}
+        TapSet[f'{S}-{D1}'] = [S,D1]
+        if D3 != S:
+          TapSet[f'{S}-{D2}'] = [S,D2]
+        if D3 != S:
+          TapSet[f'{S}-{D3}'] = [S,D3]
+        if len(list(TapSet.keys())) > 1:
+          Taps.append(TapSet)
+      else:
+        Taps.append({f'{S}-{D1}_off': None, f'{S}-{D1}_on': [S,D1]})
+        if D3 != S:
           Taps.append({f'{S}-{D2}_off': None, f'{S}-{D2}_on': [S,D2]})
+        if D3 != S:
           Taps.append({f'{S}-{D3}_off': None, f'{S}-{D3}_on': [S,D3]})
-      for S in range(Step-1, (Size>>1)-1, Step):
-        D0 = None
-        D3 = Size - S
-        D1 = ((D3 - 2) + Size) % Size
-        D2 = ((D3 - 1) + Size) % Size
-        if USE_DEMUXES:
-          Taps.append({f'{S}_Off':None, f'{S}-{D1}':[S,D1], f'{S}-{D2}':[S,D2], f'{S}-{D3}':[S,D3]})
-        else:
-          AllTaps.append([S, D1])
-          AllTaps.append([S, D2])
-          AllTaps.append([S, D3])
-          Taps.append({f'{S}-{D1}_off': None, f'{S}-{D1}_on': [S,D1]})
-          Taps.append({f'{S}-{D2}_off': None, f'{S}-{D2}_on': [S,D2]})
-          Taps.append({f'{S}-{D3}_off': None, f'{S}-{D3}_on': [S,D3]})
-      for D in range(2, Step, 1):
-        S = 0
-        if not USE_DEMUXES:
-          if not ([S,D] in AllTaps):
-            AllTaps.append([S, D])
-            Taps.append({f'{S}-{D}_off': None, f'{S}-{D}_on': [S,D]})
-      PL = ProgrammableRingGenerator(Size, Taps)
-      Aio.print(f'{USE_DEMUXES} \t{DENSE} \t{Size} \t{len(PL.getLfsrs(False))} \t{len(PL.getPolynomials())} \t{len(PL.getUsedTaps())}/{len(PL.getAllPosssibleTaps())} \t {PL.getUnusedTaps()}')
+    #print(Taps)
+    PL = ProgrammableRingGenerator(Size, Taps)
+    Aio.print(f'{USE_DEMUXES} \t{Size} \t{len(PL.getLfsrs(False))} \t{len(PL.getPolynomials())} \t{len(PL.getUsedTaps())}/{len(PL.getAllPosssibleTaps())} \t {PL.getUnusedTaps()}')
 
 
