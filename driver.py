cbf767764aa9455c04e7053718770579506446e0
Maciej Trawka <maciejtr@gmail.com>
2023-02-13 09:30:10 +0100

    3 designs

--- driver.py
+++ driver.py
@@ -2,12 +2,37 @@
 from aio import *
 # ============================
 
+# CONSTANTS ------------
+NORMAL = 1
+MINI = 2
+MICRO = 3
+
+# CONFIG ---------------
+USE_DESIGN = MICRO
+KeyAsSeed = True
+MessageLength = 4
+
+
+# ----------------------
 
 Hash = RootOfTrust.HashFunction()
 
-Hash.setLfsrIn(Lfsr(Polynomial([16, 14, 11, 8, 4, 2, 0]), TIGER_RING))
-
-Hash.setLfsrInPhaseShifter(PhaseShifter(Hash.LfsrIn, [[7], [6, 11, 14], [8, 14, 15], [5, 7, 11], [3, 4, 14], [4, 12, 14], [7, 8, 12], [10, 11, 13]]))
+if USE_DESIGN == MINI:
+    Hash.setLfsrIn(Lfsr(Polynomial([12, 10, -7, 4, -2, 0]), TIGER_RING))
+    MsgInputs = [8, 5]
+    Hash.setLfsrInPhaseShifter(PhaseShifter(Hash.LfsrIn, [[6], [6, 7, 9], [0, 1, 8], [4, 7], [4, 7, 9], [4, 7, 11], [5, 10, 11], [1, 6, 10]]))
+    Hash.Cycles = 32
+elif USE_DESIGN == MICRO:
+    Hash.setLfsrIn(Lfsr(Polynomial([8, 5, -2, 0]), TIGER_RING))
+    MsgInputs = [3, 5]
+    Hash.setLfsrInPhaseShifter(PhaseShifter(Hash.LfsrIn, [[4], [0, 1, 2], [1, 3, 7], [4, 6, 7], [2, 5, 6], [0, 2, 4], [0, 1, 7], [4, 5, 6]]))
+    Hash.Cycles = 32
+else:
+    Hash.setLfsrIn(Lfsr(Polynomial([16, 14, 11, 8, 4, 2, 0]), TIGER_RING))
+    MsgInputs = [7, 10, 12]
+    Hash.setLfsrInPhaseShifter(PhaseShifter(Hash.LfsrIn, [[7], [6, 11, 14], [8, 14, 15], [5, 7, 11], [3, 4, 14], [4, 12, 14], [7, 8, 12], [10, 11, 13]]))
+    Hash.Cycles = 32
+    
 
 Hash.addDirectConnection(1000000+0, 14)
 Hash.addDirectConnection(1000000+1, 19)
@@ -25,11 +50,10 @@ Hash.addBentFunction(BentFunction(bau.int2ba(2847997077, 1<<5, 'little')), [1000
 Hash.addBentFunction(BentFunction(bau.int2ba(1345780380, 1<<5, 'little')), [100000+28, 100000+27, 100000+26, 100000+25, 100000+24], [5])
 Hash.addBentFunction(BentFunction(bau.int2ba(1392966060, 1<<5, 'little')), [100000+5, 100000+4, 100000+3, 100000+2, 100000+1], [28])
 
-Hash.Cycles = 32
-KeyAsSeed = 1
-MessageLength = 4
 
-Result = Hash.FastANFSimulation(MessageLength, [7, 10, 12], PrintExpressionsEveryCycle=0, KeyAsSeed=KeyAsSeed)
+
+
+Result = Hash.FastANFSimulation(MessageLength, MsgInputs, PrintExpressionsEveryCycle=0, KeyAsSeed=KeyAsSeed)
 
 #print(Aio.format(Result['Expression']))
 
