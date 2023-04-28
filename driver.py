ac76e753f00f4b952c6e214542285d602ae36cf2
Maciej Trawka <maciejtr@gmail.com>
2023-04-28 18:25:55 +0200

    new

--- driver.py
+++ driver.py
@@ -3,7 +3,7 @@ from aio import *
 # ============================
 
 # load NLFSRs
-nlfsrs = NlfsrList.fromFile("../data/fpga1.txt")
+nlfsrs = NlfsrList.fromFile("../data/Found_by_software.txt")
 
 NlfsrList.toXlsDatabase(nlfsrs)
 
